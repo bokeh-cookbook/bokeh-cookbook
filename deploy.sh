bd147b28c322f0807c5d2b2cb93b3e35800da093
Sarah Bird <sarah@bonvaya.com>
2016-06-03 01:26:28 -0700

    Add come logging

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,16 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.name "Travis"
     fi
     #using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null 2>&1
+    echo -e "git clone\n"
+    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd built_website
     rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
     #add, commit and push files
     git add -f .
+    echo -e "git commit\n"
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git push -fq https://${GH_TOKEN}@github.com/$TARGET_REPO origin $BRANCH > /dev/null 2>&1
+    echo -e "git push\n"
+    git push -fq https://${GH_TOKEN}@github.com/$TARGET_REPO origin $BRANCH > /dev/null
     echo -e "Deploy completed\n"
 fi
